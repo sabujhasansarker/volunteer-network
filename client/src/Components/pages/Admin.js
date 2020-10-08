3d5ce8dd82322c9e2ae1bb396f95ce4eed131fbe
Sabuj Hasan Sarker <sabujhasansarker@gmail.com>
2020-10-08 13:11:20 +0600

    admin

--- client/src/Components/pages/Admin.js
+++ client/src/Components/pages/Admin.js
@@ -7,9 +7,12 @@ import user from "../../images/user.png";
 import add from "../../images/add.png";
 import userActive from "../../images/userActive.png";
 import addActive from "../../images/addActive.png";
+import deleteImage from "../../images/delete.png";
 
 const Admin = () => {
   const [activeHover, setActiveHover] = useState(false);
+  const table = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
   return (
     <Fragment>
       <div className="admin">
@@ -43,7 +46,32 @@ const Admin = () => {
             </ul>
           </div>
         </div>
-        <h1>Admin</h1>
+        <div className="admin-content">
+          <table className="br-13 w-100 p-10 pb-50">
+            <thead>
+              <tr className="br-13 pt-18 pb-18 pl-33 pr-33">
+                <th>Name</th>
+                <th>Email ID</th>
+                <th>Registating date</th>
+                <th>Volunteer list</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {table.map((t, i) => (
+                <tr key={i} className=" pt-21  pl-33 pr-33">
+                  <td>Sufi Ahmed Hamim</td>
+                  <td>sufi@gmail.com</td>
+                  <td>22-10-2020</td>
+                  <td>Organize books at the library.</td>
+                  <td className="bg-ff444a br-4 wpx-30 cursor-pointer">
+                    <img src={deleteImage} alt="" />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </Fragment>
   );
