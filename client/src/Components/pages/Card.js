d14832551589b23130c63fe1f9694d5e4b56b77b
Sabuj Hasan Sarker <sabujhasansarker@gmail.com>
2020-10-10 00:34:41 +0600

    get event data

--- client/src/Components/pages/Card.js
+++ client/src/Components/pages/Card.js
@@ -3,23 +3,22 @@ import React from "react";
 import card from "../../images/card.png";
 import { Link } from "react-router-dom";
 
-const Card = () => {
-  return (
-    <Link
-      to={`/registration/${Math.floor(Math.random() * 100)}`}
-      className="single-card br-10"
-    >
-      <img src={card} alt="" />
-      <div
-        className="content"
-        style={{
-          background: `#${Math.floor(Math.random() * 16777215).toString(16)}`,
-        }}
-      >
-        <h2 className="text-center  text-white f-600 fs-22">Food Charity</h2>
-      </div>
-    </Link>
-  );
+const Card = ({ event: { banner, title, _id } }) => {
+   return (
+      <Link to={`/registration/${_id}`} className="single-card br-10">
+         <img src={banner} alt="" />
+         <div
+            className="content"
+            style={{
+               background: `#${Math.floor(Math.random() * 16777215).toString(
+                  16
+               )}`,
+            }}
+         >
+            <h2 className="text-center  text-white f-600 fs-22">{title}</h2>
+         </div>
+      </Link>
+   );
 };
 
 export default Card;
--- client/src/Components/pages/Home.js
+++ client/src/Components/pages/Home.js
@@ -1,48 +1,54 @@
 import React, { Fragment } from "react";
 import Card from "./Card";
 import Navbar from "../layouts/Navbar";
+import { connect } from "react-redux";
 
-const Home = () => {
-  const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-  return (
-    <Fragment>
-      <Navbar />
-      <div className="home">
-        <div className="banner">
-          <div className="banner-overlay">
-            <div className="container pt-173 pb-203">
-              <h1 className="text-center f-700 fs-34">
-                I grow by helping people in need.
-              </h1>
-              <form action="" className="form text-center mt-26">
-                <div className="form-group">
-                  <div className="form-item">
-                    <input
-                      type="text"
-                      className="p-15 w-33 fs-16 f-500"
-                      placeholder="Search...."
-                    />
-                    <input
-                      type="submit"
-                      className="p-15 w-10 fs-16 f-500 bg-3f90fc"
-                      value="Search"
-                    />
+const Home = ({ events }) => {
+   console.log(events);
+   return (
+      <Fragment>
+         <Navbar />
+         <div className="home">
+            <div className="banner">
+               <div className="banner-overlay">
+                  <div className="container pt-173 pb-203">
+                     <h1 className="text-center f-700 fs-34">
+                        I grow by helping people in need.
+                     </h1>
+                     <form action="" className="form text-center mt-26">
+                        <div className="form-group">
+                           <div className="form-item">
+                              <input
+                                 type="text"
+                                 className="p-15 w-33 fs-16 f-500"
+                                 placeholder="Search...."
+                              />
+                              <input
+                                 type="submit"
+                                 className="p-15 w-10 fs-16 f-500 bg-3f90fc"
+                                 value="Search"
+                              />
+                           </div>
+                        </div>
+                     </form>
                   </div>
-                </div>
-              </form>
+               </div>
             </div>
-          </div>
-        </div>
-        <div className="container">
-          <div className="card-container">
-            {cards.map((c, i) => (
-              <Card key={i} />
-            ))}
-          </div>
-        </div>
-      </div>
-    </Fragment>
-  );
+            <div className="container">
+               <div className="card-container">
+                  {events &&
+                     events.map((event) => (
+                        <Card key={event._id} event={event} />
+                     ))}
+               </div>
+            </div>
+         </div>
+      </Fragment>
+   );
 };
 
-export default Home;
+const mapstatetoprops = (state) => ({
+   events: state.event.events,
+});
+
+export default connect(mapstatetoprops, {})(Home);
