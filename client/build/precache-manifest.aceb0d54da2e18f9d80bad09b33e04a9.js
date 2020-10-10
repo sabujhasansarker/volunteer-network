9fd1ee32bf8d8a37e780c5597ac9bdfcea9c6c15
Sabuj Hasan Sarker <sabujhasansarker@gmail.com>
2020-10-11 01:03:32 +0600

    verson 1.0

---
+++ client/build/precache-manifest.aceb0d54da2e18f9d80bad09b33e04a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc23ab4f3b18216d6e70b51b7d110cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5385c33b07eed7d20e7c",
+    "url": "/static/css/main.b11be8eb.chunk.css"
+  },
+  {
+    "revision": "2db5b136c2affde40b45",
+    "url": "/static/js/2.6decf95b.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/static/js/2.6decf95b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5385c33b07eed7d20e7c",
+    "url": "/static/js/main.23516b12.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "a4607ae4fece1d1a4c7790dceaa963de",
+    "url": "/static/media/banner.a4607ae4.png"
+  },
+  {
+    "revision": "e687e8681dfe6bab5cb31c3a305e89b2",
+    "url": "/static/media/card.e687e868.png"
+  }
+]);
--- server.js
+++ server.js
@@ -12,6 +12,17 @@ routers(app);
 const connectDB = require("./config/db");
 connectDB();
 
+// deploy
+// ADD THIS LINE
+if (process.env.NODE_ENV === "production") {
+   // Set static folder
+   app.use(express.static("client/build"));
+
+   app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+   });
+}
+
 // App start on port 5000
 const PORT = process.env.PORT | 5000;
 app.listen(PORT, () => console.log(`Server Start on Port ${PORT}`));
