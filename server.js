ae56fce4110843277dd244acb261203afd97b86e
Sabuj Hasan Sarker <sabujhasansarker@gmail.com>
2020-10-11 01:25:51 +0600

    verson 1.0

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ routers(app);
 const connectDB = require("./config/db");
 connectDB();
 
-// deploy
-// ADD THIS LINE
 if (process.env.NODE_ENV === "production") {
    // Set static folder
    app.use(express.static("client/build"));
@@ -24,6 +22,6 @@ if (process.env.NODE_ENV === "production") {
    });
 }
 
-// App start on port 5000
-const PORT = process.env.PORT | 5000;
-app.listen(PORT, () => console.log(`Server Start on Port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
